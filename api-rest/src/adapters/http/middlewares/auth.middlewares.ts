cfe312610610de7289b2ae4d6190c79885a89e01
Manjon2000 <albertmanjon123@gmail.com>
2023-11-28 19:48:37 +0100

    Refactor

--- api-rest/src/adapters/http/middlewares/auth.middlewares.ts
+++ api-rest/src/adapters/http/middlewares/auth.middlewares.ts
@@ -1,19 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
-import { AppDataSource } from '../../../infrastructure/database/db';
-import { User } from '../../../domain/user/user.model';
 import { exceptionsHttp } from '../exceptions';
+import { verifyEmailExists } from '../../../application/services/user/user.service';
 
 
 export const VerifyEmailMiddeleware =  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        if(req.body &&  req.body.email) {
-            const verify = await AppDataSource.getRepository(User).findOne({
-                where: {
-                    EMAIL: req.body.email
-                }
-            })
+        if(req.body && req.body.email) {
+           
+            const verifyEmail = await verifyEmailExists(req.body.email)
 
-            return (!verify ? next() : res.json(exceptionsHttp(401, 'Mail exist')));
+            return (!verifyEmail ? next() : res.json(exceptionsHttp(401, 'Mail exist')));
         }
 
         return res.json(exceptionsHttp(404, 'Required fields'))
